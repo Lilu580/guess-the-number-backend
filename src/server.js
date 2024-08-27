aabaea2a55a36998052c4f83e18b274772781dd7
lilu580 <bodia-00@ukr.net>
2024-08-27 04:21:55 +0300

    added ENV

--- src/server.js
+++ src/server.js
@@ -1,42 +1,51 @@
 const express = require('express');
-const app = express();
+const TelegramBot = require('node-telegram-bot-api');
 const cors = require('cors');
+
+require('dotenv').config();
+const {
+  TELEGRAM_BOT_TOKEN,
+  PORT,
+  WEB_APP_URL
+} = process.env
+
+const app = express();
 app.use(cors());
 app.use(express.json());
+ 
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const webAppUrl = WEB_APP_URL;
+
+  bot.sendMessage(chatId, 'Ласкаво просимо до гри "Вгадай число"! Відкрийте гру в WebView:', {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Почати гру', web_app: { url: webAppUrl } }]
+      ]
+    }
+  });
+});
 
 let secretNumber = 0;
 
 app.post('/start_game', (req, res) => {
   secretNumber = Math.floor(Math.random() * 100) + 1;
-  console.log(secretNumber)
   res.send({ message: 'Гру почато!' });
 });
 
 app.post('/guess', (req, res) => {
   const { number } = req.body;
-
-  if(!secretNumber) {
-    return res.send({message: 'Спочатку почныть гру'})
-  }
-
-  if(!number) {
-    return res.send({message: 'Щось пішло не так'})
-  }
-
   if (number == secretNumber) {
-    secretNumber = 0;
-    return res.send({ message: 'Число вгадано' });
-  }
-  
-  if (number < secretNumber) {
-    return res.send({ message: 'Загадане число більше' });
-  }
-
-  if (number > secretNumber) {
-    return res.send({ message: 'Загадане число меньше' });
+    res.send({ message: 'Число вгадано' });
+  } else if (number < secretNumber) {
+    res.send({ message: 'Загадане число більше' });
+  } else {
+    res.send({ message: 'Загадане число меньше' });
   }
 });
 
-app.listen(5000, () => {
-  console.log('Сервер запущено на порту 5000');
+app.listen(PORT, () => {
+  console.log(`Сервер запущено на порту ${PORT}`);
 });
